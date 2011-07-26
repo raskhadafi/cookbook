fdc18835dc7f9c0a43f8dcb2e763b08362ac19f2
Roman Simecek <roman.simecek@cyt.ch>
2011-07-26 11:06:32 +0200

    Inital rails 3.1 commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = '890c524ba629399ccb21f3b2a0ece376e59c5bf54f56755e699e42e28fe62d22e63ba5d09f36887de02de477eeded2bbaad05da8d141888894d512b91e888beb'
