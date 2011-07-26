ec6c2ba11a4befae212e6bf7fd11d674b8839b6d
Roman Simecek <roman.simecek@cyt.ch>
2011-07-26 13:51:34 +0200

    Recipe model with scaffold generated.

---
+++ db/migrate/20110726114322_create_recipes.rb
@@ -0,0 +1,11 @@
+class CreateRecipes < ActiveRecord::Migration
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.text :ingredients
+      t.text :instructions
+
+      t.timestamps
+    end
+  end
+end
